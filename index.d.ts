e3b5ad8dfb0442d2bf1d0e81d6dfedf7819016f3
Elian Van Cutsem <hello@elian.codes>
2023-03-14 16:56:32 +0100

    ✨ add first prototype

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module '@eliancodes/brutal-ui' {
+  type ButtonProps = {
+    href: string;
+    target: '_blank' | '_self';
+    color: string;
+    string: string;
+  }
+
+  export function Button(props: ButtonProps): any;
+}
